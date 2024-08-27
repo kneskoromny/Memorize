46f696feb3670ac442f3fd7d31dd244366d44d8e
Кирилл Нескоромный <kneskoromny@gmail.com>
2024-08-27 14:07:06 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Кирилл Нескоромный on 27.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
